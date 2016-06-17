64364c81dd1f5e4dadf37a08223c5a980d317e62
Igor Sergiienko <Igor_Sergiienko@epam.com>
2016-06-17 18:01:45 +0300

    ooo

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+const gulp = require('gulp');
+const injectfile = require("gulp-inject-file");
+const watch= require('gulp-watch');
+const open = require('gulp-open');
+
+const EMAIL = 'parts/transactional.html';
+
+gulp.task('inject', () => 
+	gulp.src(EMAIL)
+		.pipe(injectfile())
+		.pipe(gulp.dest('')
+));
+
+gulp.task('open', ['inject'], () => {
+	var options = {
+		app: 'chrome',
+		uri: 'transactional.html'
+	};
+	return gulp.src('').pipe(open(options));
+});
+
+gulp.task('watch', () => 
+	watch('parts/*.html', () => gulp.start('inject'))
+);
+
+gulp.task('default', ['open', 'watch']);
+
+
