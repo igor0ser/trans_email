40ee54038a75ac84a26fb6254d570f0a1de0e045
Igor Sergiienko <Igor_Sergiienko@epam.com>
2016-06-23 16:59:58 +0300

    outlook fix

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,27 @@ const gulp = require('gulp');
 const injectfile = require("gulp-inject-file");
 const watch= require('gulp-watch');
 const open = require('gulp-open');
+const htmlbeautify = require('gulp-html-beautify');
 
 const EMAIL = 'parts/transactional.html';
+const EMAIL_INSIDE = 'parts/inside.html';
+
+var optionsHTML = {
+	"indent_size": 1,
+	"indent_char": "	"
+	};
 
 gulp.task('inject', () => 
 	gulp.src(EMAIL)
 		.pipe(injectfile())
+		.pipe(htmlbeautify(optionsHTML))
+		.pipe(gulp.dest('')
+));
+
+gulp.task('inject2', () => 
+	gulp.src(EMAIL_INSIDE)
+		.pipe(injectfile())
+		.pipe(htmlbeautify(optionsHTML))
 		.pipe(gulp.dest('')
 ));
 
@@ -22,9 +37,9 @@ gulp.task('open', ['inject'], () => {
 });
 
 gulp.task('watch', () => 
-	watch('parts/*.html', () => gulp.start('inject'))
+	watch('parts/*.html', () => gulp.start(['inject', 'inject2']))
 );
 
-gulp.task('default', ['open', 'watch']);
+gulp.task('default', ['open', 'inject2', 'watch']);
 
 
